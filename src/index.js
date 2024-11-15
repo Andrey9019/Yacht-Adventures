b20209d511c95fa86de98a25e0f0eb22981f02ca
Andrey9019 <1234andrey5678@gmail.com>
2024-11-15 21:07:22 +0200

    update: index.js and url in package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Yacht-Adventures">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
